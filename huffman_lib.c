b3480509e39215379acde392712da188dacb4fb9
karelchanivecky <karl_chanivecky@hotmail.com>
2020-10-31 16:56:10 -0700

    completed huffman

--- huffman_lib.c
+++ huffman_lib.c
@@ -13,19 +13,15 @@
 #include <dc_utils/dlinked_list.h>
 #include <dc_utils/bit_array.h>
 #include <dc_utils/dc_tree.h>
-#include <dc/sys/stat.h>
 #include <dc/stdlib.h>
-#include <dc/fcntl.h>
 #include <dc/unistd.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 #include "stdbool.h"
-#include "fcntl.h"
 #include "limits.h"
 #include "stdio.h"
+#include "netinet/in.h"
 
-
-#define TEMP_FILENAME "huffman_temp.txt"
+#define TEMP_FILENAME "huffman_tempXXXXXXX"
 #define BYTES_PER_CHAR 1
 #define BYTES_PER_CHAR_COUNT 4
 #define BUFFER_SIZE 1
@@ -39,12 +35,35 @@ struct char_container_s {
 };
 typedef struct char_container_s char_container;
 
+void free_element( void * element, void * _ ) {
+    (void)_;
+    if ( !element ) {
+        return;
+    }
+    dc_free( &element );
+}
+
+void free_tree_contents( void * v_tree ) {
+    if ( !v_tree ) {
+        return;
+    }
+    dc_tree_node ** tree = v_tree;
+    dc_tree_map( *tree, free_element, NULL);
+    dc_tree_free_branch( tree );
+}
 char_container * create_char_container( uint8_t ch ) {
     char_container * new_char_data = ( char_container * ) dc_malloc( sizeof( struct char_container_s ));
     new_char_data->ch = ch;
     new_char_data->count = 1;
     return new_char_data;
 }
+
+char_container * create_char_container_with_count( uint8_t ch, size_t count ) {
+    char_container * char_wrapper = create_char_container( ch );
+    char_wrapper->count = count;
+    return char_wrapper;
+}
+
 
 size_t get_count_from_tree_node( void * data ) {
     dc_tree_node * tree_node = ( dc_tree_node * ) data;
@@ -75,10 +94,11 @@ int has_value_comparator( void * v_lhs, void * v_rhs ) {
 
 void add_char( dlinked_list * char_counts, uint8_t ch ) {
     char_container * ch_wrapper = create_char_container( ch );
-    size_t * search_status = NULL;
-    size_t index = dlinked_index_of_value( char_counts, ch_wrapper, has_value_comparator, search_status );
-    if ( *search_status == SUCCESS ) {
-        char_container * tgt_ch = dlinked_get_value( char_counts, index );
+    size_t index;
+    if ( dlinked_index_of_value( char_counts, dc_tree_create_node( ch_wrapper ), has_value_comparator, &index )
+         == SUCCESS ) {
+        dc_tree_node * t_ch = dlinked_get_value( char_counts, index );
+        char_container * tgt_ch = ( char_container * ) t_ch->content;
         tgt_ch->count++;
     } else {
         dlinked_push( char_counts, dc_tree_create_node( ch_wrapper ));
@@ -87,9 +107,20 @@ void add_char( dlinked_list * char_counts, uint8_t ch ) {
 
 dlinked_list * get_char_counts( int src_fd, int temp_fd ) {
     dlinked_list * char_counts = dlinked_create_list();
-    uint8_t * ch = 0;
-    while ( dc_read( src_fd, ch, BYTES_PER_CHAR ) && dc_write( temp_fd, ch, BYTES_PER_CHAR )) {
-        add_char( char_counts, *ch );
+    uint8_t ch;
+    size_t bytes_read = 0;
+    while ( dc_read( src_fd, &ch, BYTES_PER_CHAR )) {
+        bytes_read++;
+        dc_write( temp_fd, &ch, BYTES_PER_CHAR );
+        add_char( char_counts, ch );
+    }
+    if ( bytes_read == 0 ) {
+        fprintf( stderr, "File is empty!" );
+        exit( EXIT_FAILURE );
+    }
+    if ( bytes_read == 1 ) {
+        fprintf( stderr, "Cannot compress a file with only one byte!" );
+        exit( EXIT_FAILURE );
     }
     return char_counts;
 }
@@ -100,7 +131,7 @@ int compare_uint8_t( uint8_t lhs, uint8_t rhs ) {
     } else if ( lhs == rhs ) {
         return LHS_EQUAL;
     }
-    return LHS_EQUAL;
+    return LHS_LARGER;
 }
 
 int sorting_comparator( void * v_lhs, void * v_rhs ) {
@@ -131,16 +162,20 @@ dc_tree_node * make_huffman_tree( dlinked_list * ch_counts ) {
         return NULL;
     }
     if ( ch_counts->size == 1 ) {
-        return dc_tree_create_node( dlinked_pop_head( ch_counts ));
+        dc_tree_node * symbol_node = ( dc_tree_node * ) dlinked_pop_head( ch_counts );
+        char_container * root_ch_container =
+                create_char_container_with_count( 0, (( char_container * ) symbol_node->content )->count );
+        dc_tree_node * root = dc_tree_create_node( root_ch_container );
+        root->right = symbol_node;
+        return root;
     }
     while ( 1 < ch_counts->size ) {
         void * l_branch = dlinked_pop_head( ch_counts );
         void * r_branch = dlinked_pop_head( ch_counts );
         dc_tree_node * root = create_branch_node( l_branch, r_branch );
-        size_t root_value = get_count_from_tree_node( root );
         size_t insertion_index = 0;
-        int search_status = dlinked_index_of_value( ch_counts, &root_value, insertion_comparator, &insertion_index );
-        if ( search_status == FAILURE || ( search_status != BAD_ARGS && insertion_index == ch_counts->size - 1 )) {
+        int search_status = dlinked_index_of_value( ch_counts, root, insertion_comparator, &insertion_index );
+        if ( search_status == FAILURE || ( search_status != BAD_ARGS && insertion_index == ch_counts->size )) {
             dlinked_push( ch_counts, root );
         } else {
             dlinked_insert_value( ch_counts, insertion_index, root );
@@ -149,17 +184,19 @@ dc_tree_node * make_huffman_tree( dlinked_list * ch_counts ) {
     return dlinked_pop_head( ch_counts );
 }
 
-void write_char( void * v_char_wrapper ) {
-    char_container * char_wrapper = ( char_container * ) v_char_wrapper;
+void write_char( void * v_tree_node ) {
+    dc_tree_node * tree = ( dc_tree_node * ) v_tree_node;
+    char_container * char_wrapper = ( char_container * ) tree->content;
     uint8_t ch = char_wrapper->ch;
     uint32_t count = char_wrapper->count;
+    uint32_t net_count = htonl(count);
     write( STDOUT_FILENO, &ch, BYTES_PER_CHAR );
-    write( STDOUT_FILENO, &count, BYTES_PER_CHAR_COUNT );
+    write( STDOUT_FILENO, &net_count, BYTES_PER_CHAR_COUNT );
 }
 
 void write_char_counts( dlinked_list * char_counts ) {
-    dlink_map( char_counts, write_char);
-    uint8_t ch = get_char_from_tree_node(char_counts->head->content);
+    dlink_map( char_counts, write_char );
+    uint8_t ch = get_char_from_tree_node( char_counts->head->content );
     write( STDOUT_FILENO, &ch, BYTES_PER_CHAR );
 }
 
@@ -169,7 +206,7 @@ bool get_path_to_char( dc_tree_node * tree, uint8_t ch, uint8_t * path, size_t c
     }
     char_container * char_wrapper = ( char_container * ) tree->content;
     if ( char_wrapper->ch == ch ) {
-        path[ current_depth + 1 ] = END_OF_PATH;
+        path[ current_depth ] = END_OF_PATH;
         return true;
     }
     bool in_left_branch = get_path_to_char( tree->left, ch, path, current_depth + 1 );
@@ -199,19 +236,19 @@ void encode_file( int src_fd, dc_tree_node * huffman_tree ) {
     uint8_t buffer_index = 0; // keep track of how many bits have been pushed to buffer
 
     uint8_t ch = 0;
-    bool path_done = false;
+    bool path_done;
     size_t tree_height = dc_tree_height( huffman_tree ); // maximum path length
     uint8_t path[tree_height + 1]; // extra char for termination
 
-    size_t bytes_read = read( src_fd, &ch, BYTES_PER_CHAR );
+    size_t bytes_read = dc_read( src_fd, &ch, BYTES_PER_CHAR );
     while ( 0 < bytes_read ) { // while we haven't read the whole file
         get_path_to_char( huffman_tree, ch, path, 0 );
         size_t path_index = 0;
-
+        path_done = false;
         while ( !path_done ) {
             // while we have not completely processed the current path
             // write path
-            while ( path[ path_index ] != END_OF_PATH && buffer < CHAR_BIT ) {
+            while ( path[ path_index ] != END_OF_PATH && buffer_index < CHAR_BIT ) {
                 // while buffer is not full and we have not reached the end of the path
                 buffer <<= 1u;
                 buffer |= path[ path_index ];
@@ -227,10 +264,11 @@ void encode_file( int src_fd, dc_tree_node * huffman_tree ) {
             // if buffer is full, then write
             if ( buffer_index == CHAR_BIT ) {
                 write( STDOUT_FILENO, &buffer, 1 );
+                buffer = 0;
                 buffer_index = 0;
             }
         }
-        bytes_read = read( src_fd, &ch, BYTES_PER_CHAR );
+        bytes_read = dc_read( src_fd, &ch, BYTES_PER_CHAR );
     }
     // there may be some bits left in the buffer
     if ( buffer_index != 0 ) {
@@ -240,44 +278,45 @@ void encode_file( int src_fd, dc_tree_node * huffman_tree ) {
 }
 
 void huffman_compress( int src_fd ) {
-    int temp_fd = dc_open( TEMP_FILENAME, O_CREAT | O_WRONLY, 0640 );
+    char temp_filename[] = TEMP_FILENAME;
+    int temp_fd = dc_mkstemp( temp_filename );
+    off_t file_start = dc_lseek( temp_fd, 0, SEEK_CUR );
     dlinked_list * char_list = get_char_counts( src_fd, temp_fd );
-    dc_close( temp_fd );
     write_char_counts( char_list );
     dlinked_list * sorted_list = dlinked_quicksort( char_list, sorting_comparator );
-    dlinked_free_list( &char_list );
-    temp_fd = dc_open( TEMP_FILENAME, O_RDONLY );
+    dc_lseek( temp_fd, file_start, SEEK_SET );
     dc_tree_node * huffman_tree = make_huffman_tree( sorted_list );
     encode_file( temp_fd, huffman_tree );
+    dlinked_free_list( &char_list );
     dc_tree_free_branch( &huffman_tree );
     dc_close( temp_fd );
-    unlink( TEMP_FILENAME );
+    dc_unlink( temp_filename );
 }
 
-dlinked_list * parse_huffman_header( int src_fd, int temp_fd ) {
+dlinked_list * parse_huffman_header( int src_fd ) {
     dlinked_list * char_counts = dlinked_create_list();
     uint8_t header_terminator;
     uint8_t ch;
     uint32_t ch_count;
+    uint32_t net_ch_count;
     size_t bytes_read = read( src_fd, &ch, BYTES_PER_CHAR );
     header_terminator = ch;
     bool done = false;
     while ( !done && bytes_read ) {
-        write( temp_fd, &ch, BYTES_PER_CHAR );
-        bytes_read = read( src_fd, &ch_count, BYTES_PER_CHAR_COUNT );
+        bytes_read = read( src_fd, &net_ch_count, BYTES_PER_CHAR_COUNT );
         if ( bytes_read < BYTES_PER_CHAR_COUNT ) {
-            perror( "ERROR! Incorrect file size!" );
+            fprintf( stderr, "ERROR! Incorrect file size!" );
             exit( EXIT_FAILURE );
         }
-        write( temp_fd, &ch_count, BYTES_PER_CHAR_COUNT );
-        add_char( char_counts, ch );
+        ch_count = ntohl(net_ch_count);
+        dlinked_push_head( char_counts, dc_tree_create_node( create_char_container_with_count( ch, ch_count )));
         bytes_read = read( src_fd, &ch, BYTES_PER_CHAR );
         if ( ch == header_terminator ) {
             done = true;
         }
     }
     if ( !done ) {
-        perror( "ERROR! Incorrect file size!" );
+        fprintf( stderr, "ERROR! Incorrect file size!" );
         exit( EXIT_FAILURE );
     }
     return char_counts;
@@ -285,16 +324,16 @@ dlinked_list * parse_huffman_header( int src_fd, int temp_fd ) {
 
 uint8_t get_spillover_bit_count( dlinked_list * char_counts, dc_tree_node * huffman_tree ) {
     if ( char_counts->size == 0 ) {
-        perror( "Error! No compression header!" );
-        exit( EXIT_SUCCESS );
+        fprintf( stderr, "Error! No compression header!" );
+        exit( EXIT_FAILURE );
     }
     dlink * link = char_counts->head;
     size_t spillover_bits = 0;
-    size_t tree_height = dc_tree_height(huffman_tree);
+    size_t tree_height = dc_tree_height( huffman_tree );
     while ( link ) {
-        uint8_t ch = get_count_from_tree_node( link->content );
+        uint8_t ch = get_char_from_tree_node( link->content );
         size_t ch_count = get_count_from_tree_node( link->content );
-        uint8_t path[tree_height];
+        uint8_t path[tree_height + 1];
         get_path_to_char( huffman_tree, ch, path, 0 );
         spillover_bits = ( spillover_bits + ch_count * get_path_length( path )) % CHAR_BIT;
         link = link->next;
@@ -315,9 +354,10 @@ dc_tree_node * decompress_char( dc_tree_node * huffman_tree,
             current_node = current_node->left;
         }
         if ( dc_tree_is_leaf( current_node )) {
-            write( STDOUT_FILENO, &current_node->content, BYTES_PER_CHAR );
+            char_container * char_wrapper = ( char_container * ) current_node->content;
+            write( STDOUT_FILENO, &char_wrapper->ch, BYTES_PER_CHAR );
+            current_node = huffman_tree;
         }
-        current_node = huffman_tree;
     }
     return current_node;
 }
@@ -329,43 +369,38 @@ bit_array * get_bit_array( uint8_t content ) {
     return bit_arr;
 }
 
-size_t get_file_size( int fd ) {
-    struct stat st;
-    dc_fstat( fd, &st );
-    return st.st_size;
-}
-
-size_t get_current_position( int fd ) {
-    return lseek( fd, 0, SEEK_CUR );
-}
-
 void decode_file( int src_fd, dc_tree_node * huffman_tree, uint8_t spill_over_bit_count ) {
     dc_tree_node * current_node = huffman_tree;
-    uint8_t buffer;
-    size_t file_size = get_file_size( src_fd );
-    size_t cur_position = get_current_position( src_fd );
+    uint8_t next_byte;
     bit_array * bit_arr;
-    while ( cur_position < file_size ) {
-        read( src_fd, &buffer, BUFFER_SIZE );
-        bit_arr = get_bit_array( buffer );
-        current_node = decompress_char( huffman_tree, current_node, bit_arr, CHAR_BIT );
-        cur_position = get_current_position( src_fd );
+    size_t bytes_read = dc_read( src_fd, &next_byte, BUFFER_SIZE );
+    if ( bytes_read == 0 ) {
+        fprintf( stderr, "Nothing to decompress" );
     }
-    read( src_fd, &buffer, BUFFER_SIZE );
-    bit_arr = get_bit_array( buffer );
-    decompress_char( huffman_tree, current_node, bit_arr, spill_over_bit_count );
+    uint8_t byte = next_byte;
+    bytes_read = dc_read( src_fd, &next_byte, BUFFER_SIZE );
+    while ( bytes_read ) {
+        bit_arr = get_bit_array( 0 );
+        *bit_arr->array = byte;
+        current_node = decompress_char( huffman_tree, current_node, bit_arr, CHAR_BIT );
+        byte = next_byte;
+        bytes_read = dc_read( src_fd, &next_byte, BUFFER_SIZE );
+    }
+    bit_arr = get_bit_array( 0 );
+    *bit_arr->array = byte;
+    current_node = decompress_char( huffman_tree, current_node, bit_arr, spill_over_bit_count );
+    if ( current_node != huffman_tree ) {
+        fprintf( stderr, "File was not the correct size!" );
+        exit( EXIT_FAILURE );
+    }
 }
 
 void huffman_decompress( int src_fd ) {
-    int temp_fd = dc_open( TEMP_FILENAME, O_CREAT | O_RDONLY, 0640 );
-    dlinked_list * char_counts = parse_huffman_header( src_fd, temp_fd );
-    dc_close( temp_fd );
-    dc_tree_node * huffman_tree = make_huffman_tree( char_counts );
+    dlinked_list * char_counts = parse_huffman_header( src_fd );
+    dlinked_list * sorted_ch_counts = dlinked_quicksort( char_counts, sorting_comparator );
+    dc_tree_node * huffman_tree = make_huffman_tree( sorted_ch_counts );
     size_t spillover_bit_count = get_spillover_bit_count( char_counts, huffman_tree );
+    decode_file( src_fd, huffman_tree, spillover_bit_count );
+    free_tree_contents( &huffman_tree );
     dlinked_free_list( &char_counts );
-    temp_fd = dc_open( TEMP_FILENAME, O_RDONLY );
-    decode_file( temp_fd, huffman_tree, spillover_bit_count );
-    dc_tree_free_branch( &huffman_tree );
-    dc_close( temp_fd );
-    unlink( TEMP_FILENAME );
 }
