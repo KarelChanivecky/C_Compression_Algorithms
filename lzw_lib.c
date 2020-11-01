4abb4c4f80ecac9667adbb58f49f3c09301458a5
karelchanivecky <karl_chanivecky@hotmail.com>
2020-10-31 17:25:22 -0700

    completed get bit functions

--- lzw_lib.c
+++ lzw_lib.c
@@ -10,3 +10,34 @@
 //
 
 #include "lzw_lib.h"
+#include "stdint.h"
+
+uint8_t get_bits(uint16_t container, uint16_t mask, uint8_t shift) {
+    mask <<= shift;
+    return  (container & mask) >> shift;
+}
+
+uint8_t get_eight_bits(uint16_t container, uint8_t shift) {
+    return  get_bits(container, 255u, shift);
+}
+
+uint8_t get_four_bits(uint16_t container, uint8_t shift) {
+    return  get_bits(container, 15u, shift);
+}
+
+uint16_t get_four_msbs(uint16_t container) {
+    return get_four_bits(container, 8);
+}
+
+uint16_t get_four_lsbs(uint16_t container) {
+    return get_four_bits(container, 0);
+}
+
+uint16_t get_eight_msbs(uint16_t container) {
+    return get_eight_bits(container, 4);
+}
+
+uint16_t get_four_lsbs(uint16_t container) {
+    return get_eight_bits(container, 0);
+}
+
