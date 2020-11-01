5eddf8b4fe373c985c443955859fcbc0467a7269
karelchanivecky <karl_chanivecky@hotmail.com>
2020-10-31 17:06:37 -0700

    created lzw_lib

---
+++ lzw_lib.c
@@ -0,0 +1,12 @@
+/**
+ * @since 2020-10-31
+ * @author Karel Chanivecky Garcia
+ * BCIT 
+ * assignment3
+ * lzw_lib.c
+ */
+//
+// Created by karel on 2020-10-31.
+//
+
+#include "lzw_lib.h"
---
+++ lzw_lib.h
@@ -0,0 +1,16 @@
+/**
+ * @since 2020-10-31
+ * @author Karel Chanivecky Garcia
+ * BCIT 
+ * assignment3
+ * lzw_lib.h
+ */
+//
+// Created by karel on 2020-10-31.
+//
+
+#ifndef ASSIGNMENT3_LZW_LIB_H
+#define ASSIGNMENT3_LZW_LIB_H
+
+
+#endif //ASSIGNMENT3_LZW_LIB_H
